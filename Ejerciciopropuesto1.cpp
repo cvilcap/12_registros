e4491767b664259e454429ebbd511fcef8931bad
cvilcap <cvilcap@unjbg.edu.pe>
2026-06-30 22:51:46 -0500

    Ejercicio corregido y arreglado

--- Ejerciciopropuesto1.cpp
+++ Ejerciciopropuesto1.cpp
@@ -9,27 +9,40 @@ struct Empleado{
 int main(){
     int n;
     Empleado empleados[100];
+
     cout<<"Ingrese el numero de empleados: ";
     cin>>n;
     cin.ignore();
-    for(int i=0;i<n;i++){
+    if(n<=0){
+        cout<<"No hay empleados para registrar.\n";
+        return 0;
+    }
+    for(int i=0; i<n; i++){
         cout<<"Ingrese el nombre del empleado "<<i+1<<": ";
         getline(cin, empleados[i].nombre);
         cout<<"Ingrese el sexo del empleado "<<i+1<<": ";
         getline(cin, empleados[i].sexo);
-        cout<<"Ingrese el sueldo del empleado "<<i+1<<": ";
+        cout<<"Ingrese el sueldo del empleado "<<i+1<< ": ";
         cin>>empleados[i].sueldo;
         cin.ignore();
     }
-    cout<<"\nLista de empleados:\n";
-    for(int i=0;i<n;i++){
-        cout<<"Empleado "<<i+1<<":\n";
-        cout<<"Nombre: "<<empleados[i].nombre<<"\n";
-        cout<<"Sexo: "<<empleados[i].sexo<<"\n";
-        cout<<"Sueldo: "<<empleados[i].sueldo<<"\n\n";
+    int indiceMayor = 0;
+    int indiceMenor = 0;
+    for (int i = 1; i < n; i++) {
+        if (empleados[i].sueldo > empleados[indiceMayor].sueldo) {
+            indiceMayor = i;
+        }
+        if (empleados[i].sueldo < empleados[indiceMenor].sueldo) {
+            indiceMenor = i;
+        }
     }
-    for(int i=0;i<n;i++){
-        float mayor=0;
-        float menor=0;
-    }
+    cout << "\nEmpleado con mayor sueldo:\n";
+    cout << "Nombre: " << empleados[indiceMayor].nombre << "\n";
+    cout << "Sexo: " << empleados[indiceMayor].sexo << "\n";
+    cout << "Sueldo: " << empleados[indiceMayor].sueldo << "\n\n";
+    cout << "Empleado con menor sueldo:\n";
+    cout << "Nombre: " << empleados[indiceMenor].nombre << "\n";
+    cout << "Sexo: " << empleados[indiceMenor].sexo << "\n";
+    cout << "Sueldo: " << empleados[indiceMenor].sueldo << "\n";
+    return 0;
 }
